7207ec6835057cc978a7fc172d5317cc0560bf0c
smtapu99 <shahadatmahmud99@gmail.com>
2024-03-13 23:41:22 +0800

    configure gh-host

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/upwork-react-test/',
   plugins: [react()],
   server: {
     port: 3001
